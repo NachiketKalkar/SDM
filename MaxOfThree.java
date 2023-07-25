bacfa5ddcd018abb2cedf7ed772a3a2381160a89
saketautee <saketautee1997@gmail.com>
2023-07-25 08:14:40 +0530

    file added

---
+++ MaxOfThree.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class MaxOfThree{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Enter three numbers");
+        a =sc.nextInt();
+        b =sc.nextInt();
+        c=sc.nextInt();
+        int sum=a+b+c;
+        System.out.println("sum="+sum);
+    }
+}
